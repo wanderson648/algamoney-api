23c2bb95b7c5e8e77d7a727fab4b0547a0aad866
wanderson648 <testepedroca@gmail.com>
2025-08-16 13:05:01 -0300

    feat: cofigura projeto

---
+++ src/main/java/com/algaworks/algamony/api/AlgamaneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algamony.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamaneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamaneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algamony/api/AlgamaneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algamony.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamaneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
